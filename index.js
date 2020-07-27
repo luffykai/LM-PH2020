90b9f3f999fbcabdb6c9ec6da978e3843aa9bc9a
luffykai <kaikaikai1219@gmail.com>
2020-07-26 22:47:39 -0700

    fix

--- index.js
+++ index.js
@@ -1,20 +1,36 @@
-const request = require("request");
-
 const csv = require("csv-parser");
 const fs = require("fs");
+const request = require("request");
+const sync_request = require("sync-request");
 
 const FIELD_MAP = {};
 
-fs.createReadStream("data/field_map.csv")
-  .pipe(csv())
-  .on("data", (row) => (FIELD_MAP[row.ronny_field] = row.ocds_path))
-  .on("end", () => {
-    console.log(FIELD_MAP);
-  });
+const loadMap = function() {
+  fs.createReadStream("data/field_map.csv")
+    .pipe(csv())
+    .on("data", (row) => (FIELD_MAP[row.ronny_field] = row.ocds_path))
+    .on("end", () => {
+      console.log(FIELD_MAP);
+    });
+}
 
-request('https://pcc.g0v.ronny.tw/api', { json: true }, (err, res, body) => {
-  if (err) { return console.log(err); }
-  console.log(body);
-  console.log(body.url);
-  console.log(body.explanation);
-});
+const getContract = function(org_id, contract_id) {
+  res = sync_request("GET", "https://pcc.g0v.ronny.tw/api/tender?unit_id=" + org_id + "&job_number=" + contract_id);
+  return JSON.parse(res.getBody());
+}
+
+main = function() {
+  loadMap();
+  var contract = getContract2("3.80.11", "1090212-B2");
+  console.log(contract);
+  for (var release of contract.records) {
+    console.log(release.brief);
+    for (var key in release.detail) {
+      console.log(key);
+    }
+    break;
+  }
+  console.log("1");
+}
+
+main();
