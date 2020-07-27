c53a437f63edb40337de71c3e637d507a55cb146
Chi-An Wang <chianwang@fb.com>
2020-07-27 00:03:28 -0700

    make loading csv work

--- index.js
+++ index.js
@@ -3,43 +3,48 @@ const fs = require("fs");
 const request = require("request");
 const sync_request = require("sync-request");
 
-const FIELD_MAP = {};
+const parse = require("csv-parse/lib/sync");
 
-const loadMap = async function() {
+/*
+ * Load the csv file and turn it into a Map Object
+ * synchronously
+ */
+const loadMap = function () {
+  const map = new Map();
   data = fs.readFileSync("data/field_map.csv");
-  console.log(data);
-  data = csv(data);
-  console.log(data);
-  for (var row of data) {
-    FIELD_MAP[row.ronny_field] = row.ocds_path;
+  const records = parse(data, {
+    columns: true,
+    skip_empty_lines: true,
+  });
+
+  for (record of records) {
+    map.set(record.ronny_field, record.ocds_path);
   }
-  //fs.createReadStream("data/field_map.csv")
-  //  .pipe(csv())
-  //  .on("data", (row) => (FIELD_MAP[row.ronny_field] = row.ocds_path))
-  //  .on("end", () => {
-  //    console.log(FIELD_MAP);
-  //  });
-}
 
-const getContract = function(org_id, contract_id) {
-  res = sync_request("GET", "https://pcc.g0v.ronny.tw/api/tender?unit_id=" + org_id + "&job_number=" + contract_id);
+  return map;
+};
+
+const getContract = function (org_id, contract_id) {
+  res = sync_request(
+    "GET",
+    "https://pcc.g0v.ronny.tw/api/tender?unit_id=" +
+      org_id +
+      "&job_number=" +
+      contract_id
+  );
   return JSON.parse(res.getBody());
-}
+};
 
-main = function() {
-  loadMap();
-  return;
+main = function () {
+  const FIELD_MAP = loadMap();
   var contract = getContract("3.80.11", "1090212-B2");
   console.log(contract);
   for (var release of contract.records) {
     console.log(release.brief);
     for (var key in release.detail) {
-      console.log(key);
-      console.log("yo");
-      console.log(FIELD_MAP[key]);
+      console.log(FIELD_MAP.get(key));
     }
   }
-  console.log("1");
-}
+};
 
 main();
