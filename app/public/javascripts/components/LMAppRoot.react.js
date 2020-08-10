a44af7ae13fc9faece161f49d6cfa4ddb66a6047
Chi-An Wang <chianwang@fb.com>
2020-08-09 21:42:19 -0700

    how to add a component

--- app/public/javascripts/components/LMAppRoot.react.js
+++ app/public/javascripts/components/LMAppRoot.react.js
@@ -2,6 +2,8 @@
 
 var _LMMap = _interopRequireDefault(require("./LMMap.react"));
 
+var _LMTestComponent = _interopRequireDefault(require("./LMTestComponent.react"));
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 function LMAppRoot() {
@@ -21,7 +23,7 @@ function LMAppRoot() {
     className: "btn-large lm-pink-1"
   }, "I'm \u76E3\u7763\u8005"), /*#__PURE__*/React.createElement("a", {
     className: "btn-large lm-pink-1 marginLeft-8"
-  }, "I'm Officials")), /*#__PURE__*/React.createElement(_LMMap["default"], null)));
+  }, "I'm Officials")), /*#__PURE__*/React.createElement(_LMMap["default"], null), /*#__PURE__*/React.createElement(_LMTestComponent["default"], null)));
 }
 
 var domContainer = document.querySelector("#lm_app_root");
---
+++ app/src/components/LMTestComponent.react.js
@@ -0,0 +1,3 @@
+export default function LMTestComponent() {
+    return <div style={{backgroundColor: 'green', width: 10, height: 10}} />;
+}
