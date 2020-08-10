8ea7f4dc5fa226f5f45656bd220551e6ad8b567b
luffykai <kaikaikai1219@gmail.com>
2020-08-09 20:16:57 -0700

    add world map

--- app/public/javascripts/components/LMCountyRoot.react.js
+++ app/public/javascripts/components/LMCountyRoot.react.js
@@ -1,5 +1,33 @@
 "use strict";
 
+function WorldMap() {
+  const dataDiv = document.getElementById("county-map-data");
+  let worlddata = JSON.parse(dataDiv.getAttribute('data')); //worlddata = worlddata.objects.countries;
+
+  worlddata = topojson.feature(worlddata, worlddata.objects.countries).features;
+  console.log(worlddata);
+
+  const myProjection = () => {
+    return d3.geoMercator().scale(1000).center([125.9605, 26.6978]);
+  };
+
+  return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("svg", {
+    className: "map-svg",
+    width: 800,
+    height: 600,
+    viewBox: "0 0 800 600"
+  }, /*#__PURE__*/React.createElement("g", {
+    className: "countries"
+  }, worlddata.map((d, i) => /*#__PURE__*/React.createElement("path", {
+    key: `path-${i}`,
+    d: d3.geoPath().projection(myProjection())(d),
+    className: "country",
+    fill: `rgba(219, 163, 43,${1 / worlddata.length * i})`,
+    stroke: "#FFFFFF",
+    strokeWidth: 0.5
+  })))));
+}
+
 function LMCountyRoot() {
   const dataDiv = document.getElementById("county-map-data");
   const data = JSON.parse(dataDiv.getAttribute("data"));
@@ -14,8 +42,11 @@ function LMCountyRoot() {
   }
 
   console.log("data", data);
-  return /*#__PURE__*/React.createElement("div", null, "LMCountyRoot: ", county);
+  return /*#__PURE__*/React.createElement("div", null, "LMCountyRoot: ", county, "aaaaa", /*#__PURE__*/React.createElement(WorldMap, null));
 }
 
 let domContainer = document.querySelector("#lm_county_root");
-ReactDOM.render( /*#__PURE__*/React.createElement(LMCountyRoot, null), domContainer);
+ReactDOM.render( /*#__PURE__*/React.createElement(LMCountyRoot, null), domContainer); //let domContainer2 = document.querySelector('#county_map_data');
+//ReactDOM.render(<WorldMap />, domContainer2);
+
+module.exports = LMCountyRoot;
