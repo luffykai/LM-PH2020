d7981e4c991341812f523b487ee0b501534fcbcf
ken61502 <kennyson@gmail.com>
2020-08-10 22:59:33 -0400

    Fix shell script

--- list_search.sh
+++ list_search.sh
@@ -1,34 +1,35 @@
 #!/bin/sh
 
-INPUT='./data/name_query_list.tsv'
+INPUT='./data/all_projects.tsv'
 GRN=$'\e[1;32m'
 GRY=$'\e[1;37m'
 NC=$'\e[0m'
 OLDIFS=$IFS
 IFS=$'\t'
-[ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
+[ ! -f $INPUT ] && { echo "${INPUT} file not found"; exit 99; }
 [[ -d output/all ]] || mkdir output/all
 
-while read name uid title regex latlng
+while read name uid title regex_list latlng
 do
   IFS=','
-  r=""
-  for i in `echo "$regex"`
+  regex=""
+  for i in `echo "${regex_list}"`
   do
-    r="$r$i.*"
+    regex="${regex}${i}.*"
   done
-  r="$r((安置)|(社會)|(公共)|(住宅))"
-  command="node index.js search_with_unit --pid \"$name\" --t \"$title\" --uid $uid --r \"$r\""
+  regex="${regex}((安置)|(社會)|(公共)|(住宅))"
+  command="node index.js search_with_unit --pid \"${name}\" --t \"${title}\" --uid ${uid} --r \"${regex}\""
 
-  echo ""
-	printf "$GRN[ $name ]$GRY\n"
-  echo "  * Command: $command"
-  echo "  * Title: $title"
-	echo "  * UID: $uid"
-  echo "  * Regex: $r"
-  echo "$NC"
+  echo "${GRN}"
+	echo "[ ${name} ]"
+  echo "${GRY}"
+  echo "  * Command: ${command}"
+  echo "  * Title: ${title}"
+	echo "  * UID: ${uid}"
+  echo "  * Regex: ${regex}"
+  echo "${NC}"
 
-  eval $command
+  eval ${command}
   IFS=$'\t'
 done < $INPUT
 IFS=$OLDIFS
