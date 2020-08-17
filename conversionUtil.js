00aed00c55d34f37e0a26f76781e68c2dcc4c319
ken61502 <kennyson@gmail.com>
2020-08-17 16:38:31 -0400

    refactor index.js to pull some method out

---
+++ conversionUtil.js
@@ -0,0 +1,113 @@
+const stringify = require("csv-stringify");
+const sync_request = require("sync-request");
+
+const {
+  getReleaseTagFromZhString,
+  parseReleaesDateStringToIsoString,
+  initPackage,
+  postProcessing,
+} = require("./LMUtils");
+const put = require("./put.js");
+const getReleaseBuilder = require("./releaseBuilders.js");
+
+const LM_OCDS_PREFIX = "ocds-kj3ygj";
+
+const getContract = function(orgID, contractID) {
+  res = sync_request(
+    "GET",
+    `https://pcc.g0v.ronny.tw/api/tender?unit_id=${orgID}&job_number=${contractID}`
+  );
+  return JSON.parse(res.getBody());
+};
+
+// Example: 3.80.11, 1090212-B2
+const convertToOCDS = function(orgID, contractID) {
+  const ocid = `${LM_OCDS_PREFIX}-${orgID}-${contractID}`;
+  const contract = getContract(orgID, contractID);
+  releasePackage = initPackage(ocid);
+
+  const unmappedFields = {};
+
+  for (let release of contract.records) {
+    // console.log("BRIEF");
+    // console.log(release.brief);
+
+    const ocdsRelease = {};
+
+    const releaseDate = parseReleaesDateStringToIsoString(
+      release.date != null ? String(release.date) : null
+    );
+    const releaseTag = getReleaseTagFromZhString(release.brief.type);
+    // Set general information from brief
+    releaseDate && put(ocdsRelease, "date", releaseDate);
+
+    put(ocdsRelease, "ocid", ocid);
+    put(ocdsRelease, "id", `${release.filename}-${Date.parse(releaseDate)}`);
+    put(ocdsRelease, "tag[]", releaseTag);
+
+    // HardCode Data for each releases
+    put(ocdsRelease, "language", "zh");
+    put(ocdsRelease, "initiationType", "tender"); // Only tender is supported from this code list
+
+    getReleaseBuilder(releaseTag).build(
+      release.detail,
+      ocdsRelease,
+      unmappedFields
+    );
+
+    // Post-processing for ocds release
+    const processedOCDSRelease = postProcessing(ocdsRelease);
+    releasePackage.releases.push(processedOCDSRelease);
+  }
+
+  const stringifier = stringify({
+    columns: [{ key: "key" }, { key: "value" }],
+    delimiter: ","
+  });
+
+  stringify(
+    Object.entries(unmappedFields),
+    { header: true, columns: [{ key: "key" }, { key: "value" }] },
+    (err, output) => {
+      if (err) throw err;
+      fs.writeFile("./output/unmapped_fields.csv", output, err => {
+        if (err) throw err;
+        console.log("unmapped_fields.csv saved.");
+      });
+    }
+  );
+
+  return releasePackage;
+};
+
+const convertToOC4IDS = function(input) {
+  let oc4ids = {};
+  oc4ids.id = input.project_id;
+  oc4ids.title = input.project_name;
+  oc4ids.contractingProcesses = [];
+  for (let contract of input.contracts) {
+    releasePackage = convertToOCDS(contract.org_id, contract.contract_id);
+    contractProcess = {};
+    contractProcess.id = releasePackage.uri;
+    contractProcess.releases = [];
+    for (let release of releasePackage.releases) {
+      contractProcess.releases.push(release);
+    }
+    oc4ids.contractingProcesses.push(contractProcess);
+  }
+  let oc4idsPackage = {
+    uri: `ocds://contract/${input.project_id}`,
+    publishedDate: new Date().toISOString(),
+    publisher: {
+      name: "Learning Man"
+    },
+    version: "0.9",
+    projects: [oc4ids]
+  };
+  return oc4idsPackage;
+};
+
+module.exports = {
+  convertToOC4IDS,
+  convertToOCDS
+};
--- index.js
+++ index.js
@@ -2,20 +2,15 @@ const fs = require("fs");
 
 const csvparse = require("csv-parse/lib/sync");
 const admin = require("firebase-admin");
-const sync_request = require("sync-request");
 
 const argv = require("./commandsUtil.js");
 const {
-  getReleaseTagFromZhString,
-  parseReleaesDateStringToIsoString,
-  initPackage,
   outputPackage,
-  postProcessing,
   printProjectHeader,
   writeJsonFile
 } = require("./LMUtils");
-const put = require("./put.js");
-const getReleaseBuilder = require("./releaseBuilders.js");
+const { convertToOC4IDS, convertToOCDS } = require("./conversionUtil");
+
 const { convertToOc4idsInput, searchWithUnit } = require("./searchWithUnit.js");
 const serviceAccount = require("./lm-ph2020-firebase-adminsdk.json");
 
@@ -25,105 +20,6 @@ admin.initializeApp({
 });
 const db = admin.firestore();
 db.settings({ ignoreUndefinedProperties: true });
-
-const stringify = require("csv-stringify");
-
-const LM_OCDS_PREFIX = "ocds-kj3ygj";
-
-const getContract = function(orgID, contractID) {
-  res = sync_request(
-    "GET",
-    `https://pcc.g0v.ronny.tw/api/tender?unit_id=${orgID}&job_number=${contractID}`
-  );
-  return JSON.parse(res.getBody());
-};
-
-// Example: 3.80.11, 1090212-B2
-const convertToOCDS = function(orgID, contractID) {
-  const ocid = `${LM_OCDS_PREFIX}-${orgID}-${contractID}`;
-  const contract = getContract(orgID, contractID);
-  releasePackage = initPackage(ocid);
-
-  const unmappedFields = {};
-
-  for (let release of contract.records) {
-    // console.log("BRIEF");
-    // console.log(release.brief);
-
-    const ocdsRelease = {};
-
-    const releaseDate = parseReleaesDateStringToIsoString(
-      release.date != null ? String(release.date) : null
-    );
-    const releaseTag = getReleaseTagFromZhString(release.brief.type);
-    // Set general information from brief
-    releaseDate && put(ocdsRelease, "date", releaseDate);
-
-    put(ocdsRelease, "ocid", ocid);
-    put(ocdsRelease, "id", `${release.filename}-${Date.parse(releaseDate)}`);
-    put(ocdsRelease, "tag[]", releaseTag);
-
-    // HardCode Data for each releases
-    put(ocdsRelease, "language", "zh");
-    put(ocdsRelease, "initiationType", "tender"); // Only tender is supported from this code list
-
-    getReleaseBuilder(releaseTag).build(
-      release.detail,
-      ocdsRelease,
-      unmappedFields
-    );
-
-    // Post-processing for ocds release
-    const processedOCDSRelease = postProcessing(ocdsRelease);
-    releasePackage.releases.push(processedOCDSRelease);
-  }
-
-  const stringifier = stringify({
-    columns: [{ key: "key" }, { key: "value" }],
-    delimiter: ","
-  });
-
-  stringify(
-    Object.entries(unmappedFields),
-    { header: true, columns: [{ key: "key" }, { key: "value" }] },
-    (err, output) => {
-      if (err) throw err;
-      fs.writeFile("./output/unmapped_fields.csv", output, err => {
-        if (err) throw err;
-        console.log("unmapped_fields.csv saved.");
-      });
-    }
-  );
-
-  return releasePackage;
-};
-
-const convertToOC4IDS = function(input) {
-  let oc4ids = {};
-  oc4ids.id = input.project_id;
-  oc4ids.title = input.project_name;
-  oc4ids.contractingProcesses = [];
-  for (let contract of input.contracts) {
-    releasePackage = convertToOCDS(contract.org_id, contract.contract_id);
-    contractProcess = {};
-    contractProcess.id = releasePackage.uri;
-    contractProcess.releases = [];
-    for (let release of releasePackage.releases) {
-      contractProcess.releases.push(release);
-    }
-    oc4ids.contractingProcesses.push(contractProcess);
-  }
-  let oc4idsPackage = {
-    uri: `ocds://contract/${input.project_id}`,
-    publishedDate: new Date().toISOString(),
-    publisher: {
-      name: "Learning Man"
-    },
-    version: "0.9",
-    projects: [oc4ids]
-  };
-  return oc4idsPackage;
-};
 
 main = async function() {
   if (argv._.includes("search_with_unit")) {
