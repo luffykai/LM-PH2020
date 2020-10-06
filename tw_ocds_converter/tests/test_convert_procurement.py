0f83aa7456a3ec7eef820483e2c2e3a206ef9a8b
ken61502 <kennyson@gmail.com>
2020-10-06 01:31:23 -0400

    Change test names

--- tw_ocds_converter/tests/test_convert_procurement.py
+++ tw_ocds_converter/tests/test_convert_procurement.py
@@ -6,14 +6,14 @@ from tw_ocds_converter.common.release_builder import ReleaseBuilder
 from tw_ocds_converter.utils.mapping import field_mapping
 
 _TEST_SRC = os.path.dirname(os.path.realpath(__file__))
-_TENDER_TESTDATA_FILENAME = _TEST_SRC + '/testdata/tender_input.json'
+_PROCUREMENT_TESTDATA_FILENAME = _TEST_SRC + '/testdata/procurement_input.json'
 
-class TestTenderConversion(unittest.TestCase):
+class TestProcurementConversion(unittest.TestCase):
 
   @classmethod
   def setUpClass(self):
-    with open(_TENDER_TESTDATA_FILENAME) as input_json:
-      self._json = json.load(input_json)
+    with open(_PROCUREMENT_TESTDATA_FILENAME) as input_json:
+      self._json = json.load(input_json)    
 
   def test_parse(self):
     for record in self._json['records']:
